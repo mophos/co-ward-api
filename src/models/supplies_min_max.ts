6e47ffdab59c2564e6af3627847278261990c7c7
beer <beerde789@hotmail.com>
2020-03-29 05:18:00 +0700

    last

--- src/models/supplies_min_max.ts
+++ src/models/supplies_min_max.ts
@@ -11,12 +11,46 @@ export class SuppliesMinMaxModel {
   }
 
   getSuppliesMinMaxBytype(db: Knex, sub_ministry_code, ministry_code, hosptype_code) {
-    let sql = db(db('supplies as s').select('s.*', 'h.hospcode', 'h.sub_ministry_code', 'h.ministry_code','h.hosptype_code').join(db.raw('chospital as h')).as('s'))
+    let sql = db(db('supplies as s').select('s.*', 'h.hospcode', 'h.sub_ministry_code', 'h.ministry_code', 'h.hosptype_code').join(db.raw('chospital as h')).as('s'))
       .select('s.*', 'smm.id as supplies_min_max_id', 'smm.min', 'smm.max')
 
     sql.leftJoin('supplies_min_max as smm', (v) => {
       v.on('smm.supplies_id', 's.id')
     })
+    if (sub_ministry_code)
+      sql.where('sub_ministry_code', sub_ministry_code);
+    if (ministry_code)
+      sql.where('ministry_code', ministry_code);
+    if (hosptype_code)
+      sql.where('hosptype_code', hosptype_code);
+    return sql
+  }
+
+  getSuppliesMinMaxByBalance(db: Knex, hospcode) {
+    return db('view_forecast')
+      .where('hospcode', hospcode);
+  }
+
+  getSuppliesMinMaxByHosp(db: Knex, sub_ministry_code, ministry_code, hosptype_code) {
+    let sql = db('view_forecast_hosp')
+    if (sub_ministry_code)
+      sql.where('sub_ministry_code', sub_ministry_code);
+    if (ministry_code)
+      sql.where('ministry_code', ministry_code);
+    if (hosptype_code)
+      sql.where('hosptype_code', hosptype_code);
+    return sql
+  }
+
+  getSuppliesMinMaxByBalanceTotal(db: Knex, hospcode) {
+    return db('view_forecast')
+      .count().as('count')
+      .where('hospcode', hospcode);
+  }
+
+  getSuppliesMinMaxByHospTotal(db: Knex, sub_ministry_code, ministry_code, hosptype_code) {
+    let sql = db('view_forecast_hosp')
+      .count().as('count')
     if (sub_ministry_code)
       sql.where('sub_ministry_code', sub_ministry_code);
     if (ministry_code)
