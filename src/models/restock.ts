8c621ccb8d3305eb987b8396809addc9091a41d7
beer <beerde789@hotmail.com>
2020-03-29 19:12:13 +0700

    last

--- src/models/restock.ts
+++ src/models/restock.ts
@@ -3,34 +3,58 @@ import * as Knex from 'knex';
 export class RestockModel {
 
   getRestock(db: Knex, limit = 100, offset = 0) {
-    return db('restock').limit(limit).offset(offset);
+    return db('restocks').limit(limit).offset(offset);
   }
 
   getRestockTotal(db: Knex) {
-    return db('restock').count('* as count').as('count');
+    return db('restocks').count('* as count').as('count');
   }
 
   getRestockDetail(db: Knex, restockId) {
-    return db('restock_detail').where('restock_id', restockId)
+    return db('restock_details').where('restock_id', restockId)
   }
 
   getRestockDetailTotal(db: Knex, restockId) {
-    return db('restock_detail').count('* as count').where('restock_id', restockId)
+    return db('restock_details').count('* as count').where('restock_id', restockId)
   }
 
   insertRestock(db: Knex, data = {}) {
-    return db('restock')
+    return db('restocks')
       .insert(data)
   }
 
   deleteRestock(db: Knex, id) {
-    return db('restock')
+    return db('restocks')
       .delete()
       .where('id', id)
   }
 
-  insertRestockDetail(db: Knex, data = [{}]) {
-    return db('restock_detail')
+  insertRestockDetail(db: Knex, data = {}) {
+    return db('restock_details')
       .insert(data)
   }
+
+  insertRestockDetailItem(db: Knex, data = [{}]) {
+    return db('restock_detail_items')
+      .insert(data)
+  }
+
+  etSuppliesRestockByBalance(db: Knex, hospcode = undefined) {
+    let sql =  db('view_forecast')
+    if(hospcode)
+      sql.where('hospcode', hospcode);
+      return sql
+  }
+
+  getSuppliesRestockByHosp(db: Knex, sub_ministry_code = undefined, ministry_code = undefined, hosptype_code = undefined) {
+    let sql = db('view_forecast_hosp')
+    if (sub_ministry_code)
+      sql.where('sub_ministry_code', sub_ministry_code);
+    if (ministry_code)
+      sql.where('ministry_code', ministry_code);
+    if (hosptype_code)
+      sql.where('hosptype_code', hosptype_code);
+    return sql
+  }
+
 }
--- src/routes/admin/restock.ts
+++ src/routes/admin/restock.ts
@@ -16,18 +16,18 @@ router.get('/', async (req: Request, res: Response) => {
   try {
     let rs: any = await restockModel.getRestock(req.db);
     let rsTotal: any = await restockModel.getRestockTotal(req.db);
-    res.send({ ok: true, rows: rs, total: rsTotal.count, code: HttpStatus.OK });
+    res.send({ ok: true, rows: rs, total: rsTotal[0].count, code: HttpStatus.OK });
   } catch (error) {
     res.send({ ok: false, error: error.message, code: HttpStatus.OK });
   }
 });
 
 router.get('/detail/:id', async (req: Request, res: Response) => {
- const id = req.params.id
+  const id = req.params.id
   try {
     let rs: any = await restockModel.getRestockDetail(req.db, id);
     let rsTotal: any = await restockModel.getRestockDetailTotal(req.db, id);
-    res.send({ ok: true, rows: rs, total: rsTotal, code: HttpStatus.OK });
+    res.send({ ok: true, rows: rs, total: rsTotal[0].count, code: HttpStatus.OK });
   } catch (error) {
     res.send({ ok: false, error: error.message, code: HttpStatus.OK });
   }
@@ -37,24 +37,31 @@ router.get('/create', async (req: Request, res: Response) => {
   const decoded = req.decoded
   let rsHead: any
   try {
-    let data: any = await suppliesMinMaxModel.getSuppliesMinMaxByBalance(req.db);
     let head = {
-      created_at: moment().format('YYYY-MM-DD HH:MM:SS'),
       created_by: decoded.id
     }
     rsHead = await restockModel.insertRestock(req.db, head);
-    let dataSet = []
-    for (const _data of data) {
-      dataSet.push({
+    let hocp: any = await restockModel.getSuppliesRestockByHosp(req.db);
+    for (const _hocp of hocp) {
+      let hospData = {
         restock_id: rsHead,
-        hospcode: _data.hospcode,
-        supplies_id: _data.supplies_id,
-        qty: +_data.max - +_data.qty
-      })
-    }
-    let rsDetail: any = await restockModel.insertRestockDetail(req.db, dataSet);
+        hospcode: _hocp.hospcode,
+      }
+      let rsDetail: any = await restockModel.insertRestockDetail(req.db, hospData);
+      let data: any = await restockModel.etSuppliesRestockByBalance(req.db, _hocp.hospcode);
+      let dataSet = []
+      for (const _data of data) {
+        dataSet.push({
+          restock_detail_id: rsDetail,
+          supplies_id: _data.supplies_id,
+          qty: +_data.max - +_data.qty
+        })
+      }
+      await restockModel.insertRestockDetailItem(req.db, dataSet);
 
-    res.send({ ok: true, rows: rsDetail, code: HttpStatus.OK });
+    }
+    res.send({ ok: true, code: HttpStatus.OK });
+
   } catch (error) {
     console.log(error.message);
     await restockModel.deleteRestock(req.db, rsHead[0]);
