a94e5fa643f13503271c7ef3ccf9deef38483509
peemppk <peemppk5538@gmail.com>
2020-04-21 23:39:44 +0700

    push

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -110,4 +110,49 @@ router.get('/total-zone', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/bed', async (req: Request, res: Response) => {
+  const db = req.db;
+
+  try {
+    let data = [];
+    const zoneCode = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'];
+    for (const z of zoneCode) {
+      const zone: any = {};
+      zone.name = z;
+      let provinces: any = [];
+      const province: any = await model.getProvince(db, z);
+      for (const p of province) {
+        const _province: any = {};
+        _province.province_name = p.name_th;
+        const hospital: any = await model.getHospital(db, p.code)
+        const hosp = [];
+        for (const h of hospital) {
+          const _hospital: any = {};
+          _hospital.province_name = p.name_th;
+          const obj = {
+            hospital_id: h.id,
+            hospcode: h.hospcode,
+            hospname: h.hospname
+          };
+          const bed: any = await model.getBad(db, h.id)
+          for (const b of bed) {
+            obj[b.bed_name + '_qty'] = b.qty;
+            obj[b.bed_name + '_covid_qty'] = b.covid_qty;
+            obj[b.bed_name + '_usage_qty'] = b.usage_qty;
+          }
+          hosp.push(obj);
+        }
+        _province.hospitals = hosp;
+        provinces.push(_province);
+      }
+      zone.provinces = provinces;
+      data.push(zone);
+    }
+    res.send({ ok: true, rows: data, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
 export default router;
