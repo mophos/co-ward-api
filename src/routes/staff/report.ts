fc4e875ec780b0bee29a2c93ff28a370cc2ee657
peemppk <peemppk5538@gmail.com>
2020-04-21 17:57:59 +0700

    pull

--- src/routes/staff/report.ts
+++ src/routes/staff/report.ts
@@ -9,6 +9,49 @@ import * as moment from 'moment';
 const model = new ReportModel();
 const router: Router = Router();
 
+router.get('/hosp', async (req: Request, res: Response) => {
+  const db = req.db;
+
+  try {
+    let data: any = [];
+    let hops: any = [];
+    const zoneCode = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'];
+    for (const z of zoneCode) {
+      const zone:any = {};
+      zone.name = z;
+      let provinces: any = [];
+      const province: any = await model.getProvince(db, z);
+      for (const p of province) {
+        const _province:any = {};
+        _province.province_name = p.name_th;
+        const hospital: any = await model.getHospital(db, p.id)
+        const hosp = [];
+        for (const h of hospital) {
+          const obj = {
+            count: h.count,
+            gcs_id: h.gcs_id,
+            gcs_name: h.gcs_name,
+            hospital_id: h.hospital_id,
+            hospcode: h.hospcode,
+            hospname: h.hospname
+          };
+          hosp.push(obj);
+        }
+        _province.hospitals = hosp;
+          provinces.push(_province);
+        }
+      zone.provinces = provinces;
+      data.push(zone);
+    }
+    console.log(data);
+    
+    res.send({ ok: true, rows: data, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
 router.get('/zone', async (req: Request, res: Response) => {
   const db = req.db;
 
