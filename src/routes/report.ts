b2d8cd29b434a8b30779b0801558c547c29e0ce3
naphattharawat <naphattharawat@gmail.com>
2020-04-25 21:04:35 +0700

    get-suppliesg

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -564,4 +564,76 @@ router.get('/get-medicals', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/get-supplies', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+  const providerType = req.decoded.providerType;
+  const zoneCode = req.decoded.zone_code;
+  const type = req.decoded.type;
+  const _provinceCode = req.decoded.provinceCode;
+  const zone = req.query.zone;
+
+  try {
+    let zoneCodes = [];
+    let provinceCode = null;
+    if (type == 'MANAGER') {
+      if (zone) {
+        zoneCodes = [zone];
+      } else {
+        zoneCodes = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'];
+      }
+    } else {
+      if (providerType == 'ZONE') {
+        zoneCodes = [zoneCode];
+      } else if (providerType == 'SSJ') {
+        zoneCodes = [zoneCode];
+        provinceCode = _provinceCode;
+      }
+    }
+
+    let data: any = [];
+    for (const z of zoneCodes) {
+      const zone: any = {};
+      zone.name = z;
+      let provinces: any = [];
+      let province: any;
+      if (provinceCode) {
+        province = await model.getProvince(db, null, provinceCode);
+      } else {
+        province = await model.getProvince(db, z, null);
+      }
+      const hospital: any = await model.getHospital(db)
+      for (const p of province) {
+        const _province: any = {};
+        _province.province_name = p.name_th;
+        const _hosp = _.filter(hospital, { province_code: p.code })
+        const hosp = [];
+        const sup: any = await model.getSupplies(db,date)
+        for (const h of _hosp) {
+          const _hospital: any = {};
+          _hospital.province_name = p.name_th;
+          const obj = {
+            hospital_id: h.id,
+            hospcode: h.hospcode,
+            hospname: h.hospname
+          };
+          const _sup = _.filter(sup, { hospital_id: h.id })
+          for (const s of _sup) {
+            obj[s.generic_id] = s.qty;
+          }
+          hosp.push(obj);
+        }
+        _province.hospitals = hosp;
+        provinces.push(_province);
+      }
+      zone.provinces = provinces;
+      data.push(zone);
+    }
+    res.send({ ok: true, rows: data, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
 export default router;
