c032291804848593427b6cecdaba0c7c96bc6993
naphattharawat <naphattharawat@gmail.com>
2020-04-27 23:42:38 +0700

    update

--- src/models/fulfill.ts
+++ src/models/fulfill.ts
@@ -4,20 +4,43 @@ export class FullfillModel {
 
   getProducts(db: Knex, type) {
     let sql = db('wm_generics as g')
-      .select('g.generic_id', 'bg.name as generic_name', 'bh.hospname as hospital_name', 'g.qty', 'gp.min', 'gp.max',
-        'gp.safety_stock', db.raw('(gp.max-g.qty+gp.safety_stock) as fill_qty'),
-        db.raw('(gp.max-g.qty+gp.safety_stock) as recommend_fill_qty'),
-        db.raw('(g.qty*100/gp.max) as qty_order'))
+      .select('g.generic_id', 'bg.name as generic_name', 'g.hospital_id', 'bh.hospname as hospital_name', 'g.qty', 'gp.min', 'gp.max',
+        'gp.safety_stock', db.raw('(gp.max-(g.qty+ifnull(vf.qty,0))+gp.safety_stock) as fill_qty'),
+        db.raw('(gp.max-(g.qty+ifnull(vf.qty,0))+gp.safety_stock) as recommend_fill_qty'),
+        db.raw('((g.qty+ifnull(vf.qty,0))*100/gp.max) as qty_order'), db.raw(`ifnull(vf.qty,0) as reserve_qty`))
       .join('b_generic_plannings as gp', (v) => {
         v.on('g.generic_id', 'gp.generic_id');
         v.on('g.hospital_id', 'gp.hospital_id');
       })
       .join('b_generics as bg', 'bg.id', 'g.generic_id')
       .join('b_hospitals as bh', 'bh.id', 'g.hospital_id')
+      .leftJoin('view_fulfill_reserves as vf', (v) => {
+        v.on('vf.generic_id', 'g.generic_id')
+        v.on('vf.hospital_id', 'g.hospital_id')
+      })
       .where('bg.type', type)
-      .orderByRaw('g.qty*100/gp.max');
+      .orderByRaw('(g.qty+ifnull(vf.qty,0))*100/gp.max')
+      .havingRaw('fill_qty > 0')
     // console.log(sql.toString());
     return sql;
+  }
+
+  getFulFill(db: Knex) {
+    return db('wm_fulfill_drugs as fd')
+      .select('fd.*', 'u.fname', 'u.lname')
+      .join('um_users as u', 'u.id', 'fd.created_by')
+  }
+
+  getFulFillDetailItems(db: Knex,ids) {
+    return db('wm_fulfill_drug_details as fdd')
+      .select('fd.*', 'u.fname', 'u.lname')
+      .join('wm_fulfill_drug_detail_items as fddi', 'fddi.fulfull_drug_detail_id', 'fd.created_by')
+  }
+
+  approved(db: Knex, data: []) {
+    return db('wm_fulfill_drugs as fd')
+      .update('is_approved', 'Y')
+      .whereIn('id', data);
   }
 
   getHospNode(db: Knex) {
@@ -28,10 +51,11 @@ export class FullfillModel {
 
   getDrugMinMax(db: Knex, hospitalId) {
     return db('b_generics AS bg')
-      .select('bg.id as generic_id','bgp.max','bgp.min','bgp.safety_stock','bgp.hospital_id', 'bg.name as generic_name', 'bu.name as unit_name')
+      .select('bg.id as generic_id', 'bgp.max', 'bgp.min', 'bgp.safety_stock', 'bgp.hospital_id', 'bg.name as generic_name', 'bu.name as unit_name')
       .joinRaw(`LEFT JOIN b_generic_plannings AS bgp ON bgp.generic_id = bg.id AND bgp.hospital_id = ?`, hospitalId)
       .join('b_units AS bu', 'bu.id', 'bg.unit_id')
       .where('bg.type', 'DRUG')
+      .where('bg.is_actived', 'Y')
   }
 
   removeMinMax(db: Knex, hospId) {
@@ -44,5 +68,17 @@ export class FullfillModel {
       .insert(data)
   }
 
+  saveFulFillDrug(db: Knex, data) {
+    return db('wm_fulfill_drugs')
+      .insert(data);
+  }
 
+  saveFulFillDrugDetail(db: Knex, data) {
+    return db('wm_fulfill_drug_details')
+      .insert(data);
+  }
+  saveFulFillDrugDetailItem(db: Knex, data) {
+    return db('wm_fulfill_drug_detail_items')
+      .insert(data);
+  }
 }
--- src/routes/admin/fulfill.ts
+++ src/routes/admin/fulfill.ts
@@ -3,11 +3,14 @@
 import * as HttpStatus from 'http-status-codes';
 
 import * as moment from "moment"
+import { uniqBy, filter, map } from 'lodash';
 import { Router, Request, Response } from 'express';
 
 import { FullfillModel } from '../../models/fulfill';
+import { SerialModel } from '../../models/serial';
 
 const model = new FullfillModel();
+const serialModel = new SerialModel();
 const router: Router = Router();
 
 
@@ -17,6 +20,72 @@ router.get('/', async (req: Request, res: Response) => {
     let rs: any = await model.getProducts(req.db, type);
     res.send({ ok: true, rows: rs, code: HttpStatus.OK });
   } catch (error) {
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
+router.post('/drugs', async (req: Request, res: Response) => {
+  const data = req.body.data;
+  const db = req.db;
+  const userId = req.decoded.id;
+  try {
+    const head = {
+      created_by: userId,
+      code: await serialModel.getSerial(db, 'FD')
+    }
+    const fulfillId = await model.saveFulFillDrug(db, head);
+    const hospitals = uniqBy(data, 'hospital_id');
+    for (const h of hospitals) {
+      const obj: any = {
+        fulfill_drug_id: fulfillId[0],
+        hospital_id: h.hospital_id
+      }
+      const fulfillDetailId = await model.saveFulFillDrugDetail(db, obj);
+      const _data = filter(data, { 'hospital_id': h.hospital_id });
+      const items = [];
+      for (const d of _data) {
+        const item: any = {
+          fulfill_drug_detail_id: fulfillDetailId,
+          generic_id: d.generic_id,
+          qty: d.fill_qty
+        }
+        items.push(item);
+      }
+      await model.saveFulFillDrugDetailItem(db, items);
+    }
+    res.send({ ok: true, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
+router.get('/drugs', async (req: Request, res: Response) => {
+
+  const db = req.db;
+  try {
+    const rs: any = await model.getFulFill(db);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
+router.post('/drugs/approved', async (req: Request, res: Response) => {
+
+  const db = req.db;
+  const data = req.body.data;
+  try {
+    const ids = map(data, 'id');
+    const rs: any = await model.getFulFillDetailItems(db, ids);
+    await model.approved(db, ids);
+    res.send({ ok: true, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+
     res.send({ ok: false, error: error.message, code: HttpStatus.OK });
   }
 });
@@ -43,7 +112,7 @@ router.get('/min-max/get-drug-min-max', async (req: Request, res: Response) => {
 router.post('/min-max/save', async (req: Request, res: Response) => {
   const db = req.db;
   const data = req.body.data;
-  const hospId = data[0].hospital_id;  
+  const hospId = data[0].hospital_id;
 
   try {
 
