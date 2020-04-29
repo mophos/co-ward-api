b446dff4f363438e94326f1c81b356c9ca508245
beer <beerde789@hotmail.com>
2020-04-29 15:58:05 +0700

    excel rout

--- src/routes/manager/report.ts
+++ src/routes/manager/report.ts
@@ -36,12 +36,10 @@ router.get('/report2', async (req: Request, res: Response) => {
 router.get('/report3', async (req: Request, res: Response) => {
   const db = req.db;
   const date = req.query.date;
-
   try {
-    const rs: any = await model.report2(db, date);
+    const rs: any = await model.report3(db, date);
     res.send({ ok: true, rows: rs, code: HttpStatus.OK });
   } catch (error) {
-
     res.send({ ok: false, code: HttpStatus.OK });
   }
 });
@@ -137,5 +135,132 @@ router.get('/report10', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/report1/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report2/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report3/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+  try {
+    const rs: any = await model.report3(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report4/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report5/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report6/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report7/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report8/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report9/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
+
+router.get('/report10/excel', async (req: Request, res: Response) => {
+  const db = req.db;
+  const date = req.query.date;
+
+  try {
+    const rs: any = await model.report2(db, date);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+
+    res.send({ ok: false, code: HttpStatus.OK });
+  }
+});
 
 export default router;
