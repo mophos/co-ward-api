97139ec2f9ac06de47a7f1cb9e21f5864ae23d15
gun khemnak <gunnn_boy_hand@hotmail.com>
2020-05-20 14:52:35 +0700

    push

--- src/routes/staff/request-product.ts
+++ src/routes/staff/request-product.ts
@@ -23,6 +23,18 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/details/:id', async (req: Request, res: Response) => {
+  const db = req.db;
+  const id = req.params.id;
+
+  try {
+    let rs: any = await model.getlistDetails(db, id);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
 router.get('/product-types', async (req: Request, res: Response) => {
   let db = req.db;
   try {
@@ -40,6 +52,38 @@ router.get('/products', async (req: Request, res: Response) => {
     let rs: any = await model.getProducts(db, typeId);
     res.send({ ok: true, rows: rs, code: HttpStatus.OK });
   } catch (error) {
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+});
+
+router.post('/', async (req: Request, res: Response) => {
+  let db = req.db;
+  const data = req.body.data;
+  const hospitalId = req.decoded.hospitalId;
+  const userId = req.decoded.id;
+
+  try {
+    const h: any = {};
+    h.code = await serialModel.getSerial(req.db, 'RP');
+    h.hospital_id = hospitalId;
+    h.created_by = userId;
+    const requestId = await model.save(db, h);
+
+    const _data: any = [];
+    for (const v of data) {
+      const d: any = {};
+      d.request_id = requestId[0];
+      d.product_id = v.product_id;
+      d.qty = v.qty;
+      d.created_by = userId;
+      _data.push(d);
+    }
+    await model.saveDetails(db, _data);
+
+    res.send({ ok: true, code: HttpStatus.OK });
+  } catch (error) {
+    console.log(error);
+
     res.send({ ok: false, error: error.message, code: HttpStatus.OK });
   }
 });
