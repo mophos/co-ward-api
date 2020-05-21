21919eac898f24c0913172c3eba94b7086b69244
naphattharawat <naphattharawat@gmail.com>
2020-05-21 16:07:24 +0700

    update

--- src/models/covid-case.ts
+++ src/models/covid-case.ts
@@ -524,13 +524,13 @@ export class CovidCaseModel {
 
   listOldPatient(db: Knex, hospitalId) {
     const sql = db('views_covid_case_last as cl')
-    .select('p.hn','pc.an','pp.first_name','pp.last_name','p.id as patient_id','pc.id as covid_case_id')
-    .join('p_covid_cases as pc', 'pc.id', 'cl.covid_case_id')
-    .join('p_patients as p', 'p.id', 'pc.patient_id')
+      .select('p.hn', 'pc.an', 'pp.first_name', 'pp.last_name', 'p.id as patient_id', 'pc.id as covid_case_id')
+      .join('p_covid_cases as pc', 'pc.id', 'cl.covid_case_id')
+      .join('p_patients as p', 'p.id', 'pc.patient_id')
       .join('p_persons as pp', 'pp.id', 'p.person_id')
       .where('cl.hospital_id', hospitalId)
-      .whereNull('cl.gcs_id');
-    console.log(sql.toString());
+      .whereNull('cl.gcs_id')
+      .where('pc.is_deleted', 'N');
     return sql;
 
   }
