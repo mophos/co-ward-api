e3b979813c38f5a71e30f0ddd39eadd3fd1f850c
pinmora <nawaphol.pmr@hotmail.com>
2020-07-10 15:02:38 +0700

    ทึกอาการแพ้

--- src/models/hpvc.ts
+++ src/models/hpvc.ts
@@ -3,12 +3,18 @@ import * as Knex from 'knex';
 export class HpvcModel {
 
   getList(db: Knex, personId) {
-    return db('p_hpvc as h')
-      .join('p_hpvc_details as hd', 'hd.p_hpvc_id', 'h.id')
-      .join('b_hpvc as bh', 'bh.id', 'hd.hpvc_id')
+    let sql = db('p_hpvc as h')
+      .leftJoin('p_hpvc_details as hd', 'hd.p_hpvc_id', 'h.id')
+      .leftJoin('b_hpvc as bh', 'bh.id', 'hd.hpvc_id')
+      .leftJoin('p_hpvc_drug_details as dhd', 'dhd.p_hpvc_id', 'h.id')
+      .leftJoin('b_generics as g', 'g.id', 'dhd.drug_id')
       .leftJoin('um_users as u', 'u.id', 'h.created_by')
-      .select('h.created_date', 'u.fname', db.raw(`group_concat(bh.name) as hpvc_name`))
+      .select('h.id', 'h.created_date', 'u.fname', db.raw(`group_concat( DISTINCT bh.name, ' ') as hpvc_name`), db.raw(`group_concat(DISTINCT g.name, ' ') as drug_name`))
       .where('h.person_id', personId)
+      .groupBy('h.id')
+      console.log(sql.toString());
+      
+      return sql
     // .orderBy('id', 'DESC')
   }
 
@@ -16,6 +22,35 @@ export class HpvcModel {
     return db('b_generics')
       .where('type', 'DRUG')
       .where('is_deleted', 'N')
+  }
+
+  saveHpvc(db: Knex, data) {
+    return db('p_hpvc')
+      .insert(data);
+  }
+  saveHpvcDetail(db: Knex, data) {
+    return db('p_hpvc_details')
+      .insert(data);
+  }
+  saveHpvcDrugDetail(db: Knex, data) {
+    return db('p_hpvc_drug_details')
+      .insert(data);
+  }
+
+  deleteHpvc(db: Knex, id) {
+    return db('p_hpvc')
+      .delete()
+      .where('id', id);
+  }
+  deleteHpvcDetail(db: Knex, id) {
+    return db('p_hpvc_details')
+      .delete()
+      .where('p_hpvc_id', id);
+  }
+  deleteHpvcDrugDetail(db: Knex, id) {
+    return db('p_hpvc_drug_details')
+      .delete()
+      .where('p_hpvc_id', id);
   }
 
 }
--- src/routes/staff/hpvc.ts
+++ src/routes/staff/hpvc.ts
@@ -6,6 +6,7 @@ import { Router, Request, Response } from 'express';
 
 import { HpvcModel } from '../../models/hpvc';
 
+import { map } from 'lodash';
 const model = new HpvcModel();
 const router: Router = Router();
 const request = require("request");
@@ -27,6 +28,49 @@ router.get('/products', async (req: Request, res: Response) => {
   } catch (error) {
     res.send({ ok: false, error: error.message, code: HttpStatus.OK });
   }
+});
+
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const userId = req.decoded.id;
+    const personId = req.body.personId;
+    const hpvcId = req.body.hpvcId;
+    const drugId = req.body.drugId;
+    console.log(userId,personId,hpvcId,drugId);
+    
+    const rs: any = await model.saveHpvc(req.db, { created_by: userId, person_id: personId });
+    const hpvc = map(hpvcId, (v: any)=> {
+      return {
+        p_hpvc_id: rs[0],
+        hpvc_id: v
+      }
+    })
+    const drug = map(drugId, (v: any)=> {
+      return {
+        p_hpvc_id: rs[0],
+        drug_id: v
+      }
+    })
+    console.log(drug);
+    
+    await model.saveHpvcDetail(req.db, hpvc);
+    await model.saveHpvcDrugDetail(req.db, drug);
+    res.send({ ok: true, rows: rs, code: HttpStatus.OK });
+  } catch (error) {
+    res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+  }
+
+  router.delete('/:id', async (req: Request, res: Response) => {
+    try {
+      let id = req.params.id
+      await model.deleteHpvcDrugDetail(req.db, id);
+      await model.deleteHpvcDetail(req.db, id);
+      await model.deleteHpvc(req.db, id);
+      res.send({ ok: true, code: HttpStatus.OK });
+    } catch (error) {
+      res.send({ ok: false, error: error.message, code: HttpStatus.OK });
+    }
+  });
 });
 
 
