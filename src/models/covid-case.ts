02458de33f52b065a27b35e6b06e73924d032e33
naphattharawat <naphattharawat@gmail.com>
2021-01-05 16:47:20 +0700

    update entrydate

--- src/models/covid-case.ts
+++ src/models/covid-case.ts
@@ -77,7 +77,7 @@ export class CovidCaseModel {
       .as('updated_entry')
 
     const sql = db('p_covid_cases as c')
-      .select('c.updated_entry', 'c.id as covid_case_id', 'c.status', 'c.date_admit', 'pt.hn', 'pt.person_id', 'cd.id as covid_case_details_id', 'p.*', 't.name as title_name',
+      .select('ccd.updated_entry', 'c.id as covid_case_id', 'c.status', 'c.date_admit', 'pt.hn', 'pt.person_id', 'cd.id as covid_case_details_id', 'p.*', 't.name as title_name',
         'ccd.bed_id', 'ccd.gcs_id', 'cd.medical_supplie_id', db.raw(`ifnull(cd.create_date, null) as create_date`),
         db.raw(`ifnull(cd.entry_date, null) as entry_date`),
       //   db.raw(`(select generic_id from p_covid_case_detail_items where covid_case_detail_id = ccd.covid_case_detail_id and (generic_id = 1 or generic_id = 2) limit 1) as set1,
