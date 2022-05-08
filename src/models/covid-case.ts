15131db1af97c276de0bfa7e2078506e4bb88589
nawaphol <nawaphol.pmr@hotmail.com>
2022-05-08 22:14:20 +0700

    edit model getgcs

--- src/models/covid-case.ts
+++ src/models/covid-case.ts
@@ -588,6 +588,7 @@ export class CovidCaseModel {
       .join('p_covid_case_details as cd', 'cd.id', 'v.covid_case_detail_id')
       .join('p_patients as p', 'p.id', 'c.patient_id')
       .where('c.status', 'ADMIT')
+      .where('c.is_deleted', 'N')
       .where('p.hospital_id', hospitalId)
       .groupBy('cd.gcs_id').as('bh')
 
